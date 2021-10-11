d7d429dfac0d14cec0194d96897b22c08be22f24
Roch Bertucat <roch.bertucat@engisis.com>
2021-10-11 12:39:54 +0200

    Bump web-ifc-viewer and mui versions and fix related files

--- react-using-hooks-with-ts/src/App.tsx
+++ react-using-hooks-with-ts/src/App.tsx
@@ -1,37 +1,14 @@
-import React, {  useEffect, useRef, useState } from 'react';
-import {IconButton, createStyles, makeStyles, CircularProgress, Backdrop} from '@material-ui/core';
+import React, { useEffect, useRef, useState } from 'react';
+
+import { IconButton, CircularProgress, Backdrop } from '@mui/material';
 import { IfcViewerAPI } from 'web-ifc-viewer';
-import FolderOpenOutlinedIcon from '@material-ui/icons/FolderOpenOutlined';
+import { FolderOpenOutlined } from '@mui/icons-material';
 
-const useStyle = makeStyles(() => createStyles({
-
-  contentWrapper: {
-    display: 'flex',
-    flexDirection: 'row',
-    height: '100vh',
-    overflow: 'hidden'
-  },
-  ifcContainer: {
-    position: 'relative',
-    height: '100%',
-    width: '100%'
-  },
-  fileInput: {
-    display: 'none',
-  },
-  backdrop:{
-    zIndex: 100,
-    display: "flex",
-    alignItems: "center",
-    alignContent: "center"
-  }
-}));
 function App() {
-  const classes = useStyle();
 
   const ifcContainer = useRef<HTMLDivElement | null>(null);
   const [viewer, setViewer] = useState<IfcViewerAPI | null>(null);
-  const [loading, setLoading] =useState(false)
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     if (ifcContainer.current) {
@@ -45,40 +22,40 @@ function App() {
   useEffect(() => {
     if (viewer) {
       viewer.IFC.setWasmPath('../../');
-      window.addEventListener('mousemove', viewer.IFC.pickIfcItem);
+      window.onmousemove = viewer.IFC.prePickIfcItem;
     }
   }, [viewer]);
   return (
-      <div className={classes.contentWrapper}>
-        <div>
-          <input
-              type="file"
-              id="file"
-              accept='.ifc'
-              onChange={async (e): Promise<void> => {
-                const file =e?.target?.files!==null&& e?.target?.files[0];
-                if (file) {
-                  setLoading(true);
-                  await viewer?.IFC.loadIfc(file, true);
-                  setLoading(false)
-                }
-              }}
-              className={classes.fileInput}
-          />
-          <label htmlFor="file">
-            <IconButton color="primary" aria-label="upload file" component="span">
-              <FolderOpenOutlinedIcon />
-            </IconButton>
-          </label>
-        </div>
-        <div className={classes.ifcContainer} ref={ifcContainer}/>
-        <Backdrop
-            className={classes.backdrop}
-            open={loading}
-        >
-          <CircularProgress/>
-        </Backdrop>
+    <div className={'contentWrapper'}>
+      <div>
+        <input
+          type="file"
+          id="file"
+          accept='.ifc'
+          onChange={async (e): Promise<void> => {
+            const file = e?.target?.files !== null && e?.target?.files[0];
+            if (file) {
+              setLoading(true);
+              await viewer?.IFC.loadIfc(file, true);
+              setLoading(false)
+            }
+          }}
+          className={'fileInput'}
+        />
+        <label htmlFor="file">
+          <IconButton color="primary" aria-label="upload file" component="span">
+            <FolderOpenOutlined />
+          </IconButton>
+        </label>
       </div>
+      <div className={'ifcContainer'} ref={ifcContainer} />
+      <Backdrop
+        className={'backdrop'}
+        open={loading}
+      >
+        <CircularProgress />
+      </Backdrop>
+    </div>
   );
 }
 
