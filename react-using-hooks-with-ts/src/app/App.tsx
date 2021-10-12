471d9eec7f7e228157f2194785acd820ccbde05a
Roch Bertucat <roch.bertucat@engisis.com>
2021-10-12 12:34:24 +0200

    Add a AppBar and a Drawer

--- react-using-hooks-with-ts/src/app/App.tsx
+++ react-using-hooks-with-ts/src/app/App.tsx
@@ -1,60 +1,248 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, createRef, useState } from 'react';
 
-import { IconButton, CircularProgress, Backdrop } from '@mui/material';
+import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import {
+  Backdrop,
+  Box,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Divider,
+  CircularProgress,
+  CssBaseline,
+  IconButton,
+  Toolbar,
+  Link,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from '@mui/material';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { FolderOpenOutlined, CompareArrowsSharp, HelpOutline, GitHub } from '@mui/icons-material';
+
 import { IfcViewerAPI } from 'web-ifc-viewer';
-import { FolderOpenOutlined } from '@mui/icons-material';
+import { IfcContainer } from './IfcContainer';
+
+const drawerWidth = 240;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
 
 function App() {
+  const theme = useTheme();
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
+  const [isDialogOpen, setDialogOpen] = useState(false);
+  const [test, setTest] = useState(false);
 
-  const ifcContainer = useRef<HTMLDivElement | null>(null);
-  const [viewer, setViewer] = useState<IfcViewerAPI | null>(null);
-  const [loading, setLoading] = useState(false)
+  const [isLoading, setLoading] = useState(false)
+
+  const ifcContainer = createRef<HTMLDivElement>();
+  const [viewer, setViewer] = useState<IfcViewerAPI>();
 
   useEffect(() => {
     if (ifcContainer.current) {
-      const ifcContainerElement = ifcContainer.current;
-      const ifcViewer = new IfcViewerAPI({ container: ifcContainerElement });
+      const container = ifcContainer.current;
+      const ifcViewer = new IfcViewerAPI({ container });
       ifcViewer.addAxes();
       ifcViewer.addGrid();
+      ifcViewer.IFC.loader.ifcManager.applyWebIfcConfig({
+        COORDINATE_TO_ORIGIN: true,
+        USE_FAST_BOOLS: false
+      });
+      ifcViewer.IFC.loader.manager
       setViewer(ifcViewer);
     }
   }, []);
-  useEffect(() => {
-    if (viewer) {
-      window.onmousemove = viewer.IFC.prePickIfcItem;
+
+  const ifcOnLoad = async (e) => {
+    const file = e && e.target && e.target.files && e.target.files[0];
+    if (file && viewer) {
+      setLoading(true);
+      await viewer.IFC.loadIfc(file, true);
+      setLoading(false)
     }
-  }, [viewer]);
+  };
+
+  const toggleClippingPlanes = () => {
+    if (viewer) {
+      viewer.toggleClippingPlanes();
+      if (viewer.clipper.active) {
+        setTest(true);
+      } else {
+        setTest(false);
+      }
+    }
+  }
+
   return (
-    <div className={'contentWrapper'}>
-      <div>
-        <input
-          type="file"
-          id="file"
-          accept='.ifc'
-          onChange={async (e): Promise<void> => {
-            const file = e?.target?.files !== null && e?.target?.files[0];
-            if (file) {
-              setLoading(true);
-              await viewer?.IFC.loadIfc(file, true);
-              setLoading(false)
-            }
-          }}
-          className={'fileInput'}
-        />
-        <label htmlFor="file">
-          <IconButton color="primary" aria-label="upload file" component="span">
-            <FolderOpenOutlined />
-          </IconButton>
-        </label>
-      </div>
-      <div className={'ifcContainer'} ref={ifcContainer} />
+    <>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position='fixed' open={isDrawerOpen}>
+          <Toolbar>
+            <IconButton
+              color='inherit'
+              aria-label='open drawer'
+              onClick={() => setDrawerOpen(true)}
+              edge='start'
+              sx={{
+                marginRight: '36px',
+                ...(isDrawerOpen && { display: 'none' }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant='h6' noWrap component='div'>
+              Ifc.js React MUI Viewer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant='permanent' open={isDrawerOpen}>
+          <DrawerHeader>
+            <IconButton onClick={() => setDrawerOpen(false)}>
+              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          <List>
+            <input
+              type='file'
+              id='file'
+              accept='.ifc'
+              onChange={ifcOnLoad}
+              style={{ display: 'none' }}
+            />
+            <label htmlFor='file'>
+              <ListItem button key={'openFile'}>
+                <ListItemIcon>
+                  <FolderOpenOutlined />
+                </ListItemIcon>
+                <ListItemText primary={'Open File'} />
+              </ListItem>
+            </label>
+            <ListItem button key={'showPlane'} onClick={() => toggleClippingPlanes()}
+              selected={test}>
+              <ListItemIcon>
+                <CompareArrowsSharp />
+              </ListItemIcon>
+              <ListItemText primary={'Clipping Planes'} />
+            </ListItem>
+          </List>
+          <Divider />
+          <List>
+            <ListItem button key={'About'} onClick={() => setDialogOpen(true)} >
+              <ListItemIcon>
+                <HelpOutline />
+              </ListItemIcon>
+              <ListItemText primary={'About'} />
+            </ListItem>
+          </List>
+        </Drawer>
+        <Box component='main' sx={{ flexGrow: 1 }}>
+          <DrawerHeader />
+          <IfcContainer
+            ref={ifcContainer}
+            viewer={viewer} />
+        </Box>
+      </Box>
+
       <Backdrop
-        className={'backdrop'}
-        open={loading}
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={isLoading}
       >
-        <CircularProgress />
+        <CircularProgress color='inherit' />
       </Backdrop>
-    </div>
+
+      <Dialog onClose={() => setDialogOpen(false)} open={isDialogOpen}>
+        <DialogTitle>About</DialogTitle>
+        <DialogContent>
+          <List dense>
+            <ListItem>
+              <ListItemText
+                primary='right-click' secondary='Create a Plan'
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary='double-click' secondary='Pick an Item'
+              />
+            </ListItem>
+          </List>
+          <Link href='https://github.com/IFCjs' underline='hover' target='_blank'>
+            Join us on GitHub
+          </Link>
+          <GitHub />
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
 
